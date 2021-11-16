0fb73f2e5d2dd31724f21ee8c0015e8e16948d13
Brandon-Dayta <86785873+Brandon-Dayta@users.noreply.github.com>
2021-11-16 11:51:30 -0600

    Update cart-shipping.php

--- woocommerce/cart/cart-shipping.php
+++ woocommerce/cart/cart-shipping.php
@@ -12,82 +12,72 @@
  * happen. When this occurs the version of the template file will be bumped and
  * the readme will list any important changes.
  *
- * @see 	    https://docs.woocommerce.com/document/template-structure/
- * @author 		WooThemes
- * @package 	WooCommerce/Templates
- * @version     3.2.0
+ * @see https://docs.woocommerce.com/document/template-structure/
+ * @package WooCommerce\Templates
+ * @version 3.6.0
  */
-if ( ! defined( 'ABSPATH' ) ) {
-	exit;
-}
+
+defined( 'ABSPATH' ) || exit;
+
+$formatted_destination    = isset( $formatted_destination ) ? $formatted_destination : WC()->countries->get_formatted_address( $package['destination'], ', ' );
+$has_calculated_shipping  = ! empty( $has_calculated_shipping );
+$show_shipping_calculator = ! empty( $show_shipping_calculator );
+$calculator_text          = '';
 ?>
-<tr class="shipping">
+<tr class="woocommerce-shipping-totals shipping">
 	<th><?php echo wp_kses_post( $package_name ); ?></th>
 	<td data-title="<?php echo esc_attr( $package_name ); ?>">
-
-
-
-		<?php if ( 1 < count( $available_methods ) ) : ?>
-
-			<?php
-			$chosen_methods = WC()->session->get( 'chosen_shipping_methods' );
-			$chosen_shipping = $chosen_methods[0];
-			if( !empty($chosen_shipping)){
-				$checked = 'checked';
-			}else{
-				$checked = '';
-			}
-			?>
-			<div class="prepaid_shipping">
-				<input type="checkbox" id="s_prepaid_shipping" <?php echo $checked;?> name="s_prepaid_shipping"><label for="s_prepaid_shipping">Prepaid shipping</label>
-			</div>
-			<ul id="shipping_method">
+		<?php if ( $available_methods ) : ?>
+			<ul id="shipping_method" class="woocommerce-shipping-methods">
 				<?php foreach ( $available_methods as $method ) : ?>
 					<li>
 						<?php
-							printf( '<input type="radio" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" %4$s />
-								<label for="shipping_method_%1$d_%2$s">%5$s</label>',
-								$index, sanitize_title( $method->id ), esc_attr( $method->id ), checked( $method->id, $chosen_method, false ), wc_cart_totals_shipping_method_label( $method ) );
-
-							do_action( 'woocommerce_after_shipping_rate', $method, $index );
+						if ( 1 < count( $available_methods ) ) {
+							printf( '<input type="radio" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" %4$s />', $index, esc_attr( sanitize_title( $method->id ) ), esc_attr( $method->id ), checked( $method->id, $chosen_method, false ) ); // WPCS: XSS ok.
+						} else {
+							printf( '<input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d_%2$s" value="%3$s" class="shipping_method" />', $index, esc_attr( sanitize_title( $method->id ) ), esc_attr( $method->id ) ); // WPCS: XSS ok.
+						}
+						printf( '<label for="shipping_method_%1$s_%2$s">%3$s</label>', $index, esc_attr( sanitize_title( $method->id ) ), wc_cart_totals_shipping_method_label( $method ) ); // WPCS: XSS ok.
+						do_action( 'woocommerce_after_shipping_rate', $method, $index );
 						?>
 					</li>
 				<?php endforeach; ?>
 			</ul>
-			<div class="shipping_account">
-				<label>Please indicate your shipping account number <span class="shipping_span">*</span> <span class="optional" style="display: none;">(optional, information not required)</span></label>
-				<input type="text" class="s_shipping_account" name="s_shipping_account" value="">
-				<label>Please indicate your shipping method <span class="shipping_span">*</span> <span class="optional" style="display: none;">(optional, information not required)</span></label>
-				<input type="text" class="s_shipping_method" name="s_shipping_method" value="">
-				<p>Customer is responsible for duty tax, value added tax and shipping cost.</p>
-			</div>
-
-		<?php elseif ( 1 === count( $available_methods ) ) :  ?>
+			<?php if ( is_cart() ) : ?>
+				<p class="woocommerce-shipping-destination">
+					<?php
+					if ( $formatted_destination ) {
+						// Translators: $s shipping destination.
+						printf( esc_html__( 'Shipping to %s.', 'woocommerce' ) . ' ', '<strong>' . esc_html( $formatted_destination ) . '</strong>' );
+						$calculator_text = esc_html__( 'Change address', 'woocommerce' );
+					} else {
+						echo wp_kses_post( apply_filters( 'woocommerce_shipping_estimate_html', __( 'Shipping options will be updated during checkout.', 'woocommerce' ) ) );
+					}
+					?>
+				</p>
+			<?php endif; ?>
 			<?php
-				$method = current( $available_methods );
-				printf( '%3$s <input type="hidden" name="shipping_method[%1$d]" data-index="%1$d" id="shipping_method_%1$d" value="%2$s" class="shipping_method" />', $index, esc_attr( $method->id ), wc_cart_totals_shipping_method_label( $method ) );
-				do_action( 'woocommerce_after_shipping_rate', $method, $index );
-			?>
-			<div class="shipping_account">
-				<input type="hidden" class="ss_prepaid_shipping" name="ss_prepaid_shipping" value="1">
-				<label>Please indicate your shipping account number <span class="shipping_span">*</span></label>
-				<input type="text" required class="s_shipping_account" name="s_shipping_account" value="">
-				<label>Please indicate your shipping method <span class="shipping_span">*</span></label>
-				<input type="text" required class="s_shipping_method" name="s_shipping_method" value="">
-				<p>Customer is responsible for duty tax, value added tax and shipping cost.</p>
-			</div>
-		<?php elseif ( WC()->customer->has_calculated_shipping() ) : ?>
-			<?php echo apply_filters( is_cart() ? 'woocommerce_cart_no_shipping_available_html' : 'woocommerce_no_shipping_available_html', wpautop( __( 'There are no shipping methods available. Please ensure that your address has been entered correctly, or contact us if you need any help.', 'woocommerce' ) ) ); ?>
-		<?php elseif ( ! is_cart() ) : ?>
-			<?php echo wpautop( __( 'Enter your full address to see shipping costs.', 'woocommerce' ) ); ?>
-		<?php endif; ?>
+		elseif ( ! $has_calculated_shipping || ! $formatted_destination ) :
+			if ( is_cart() && 'no' === get_option( 'woocommerce_enable_shipping_calc' ) ) {
+				echo wp_kses_post( apply_filters( 'woocommerce_shipping_not_enabled_on_cart_html', __( 'Shipping costs are calculated during checkout.', 'woocommerce' ) ) );
+			} else {
+				echo wp_kses_post( apply_filters( 'woocommerce_shipping_may_be_available_html', __( 'Enter your address to view shipping options.', 'woocommerce' ) ) );
+			}
+		elseif ( ! is_cart() ) :
+			echo wp_kses_post( apply_filters( 'woocommerce_no_shipping_available_html', __( 'There are no shipping options available. Please ensure that your address has been entered correctly, or contact us if you need any help.', 'woocommerce' ) ) );
+		else :
+			// Translators: $s shipping destination.
+			echo wp_kses_post( apply_filters( 'woocommerce_cart_no_shipping_available_html', sprintf( esc_html__( 'No shipping options were found for %s.', 'woocommerce' ) . ' ', '<strong>' . esc_html( $formatted_destination ) . '</strong>' ) ) );
+			$calculator_text = esc_html__( 'Enter a different address', 'woocommerce' );
+		endif;
+		?>
 
 		<?php if ( $show_package_details ) : ?>
 			<?php echo '<p class="woocommerce-shipping-contents"><small>' . esc_html( $package_details ) . '</small></p>'; ?>
 		<?php endif; ?>
 
-		<?php if ( ! empty( $show_shipping_calculator ) ) : ?>
-			<?php woocommerce_shipping_calculator(); ?>
+		<?php if ( $show_shipping_calculator ) : ?>
+			<?php woocommerce_shipping_calculator( $calculator_text ); ?>
 		<?php endif; ?>
 	</td>
 </tr>
